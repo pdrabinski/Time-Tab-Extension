3fe4c3673343f805b7e11916d31a31c02a613cff
pdrabinski <pauldrabinski@gmail.com>
2018-06-24 22:35:07 -0700

    overwrite eliminated

--- game.js
+++ game.js
@@ -8,107 +8,115 @@ $(document).ready(function() {
     moves: 1,
 };
 
-$("#xbutton").click({param1: 'x'}, setFig);
-$("#obutton").click({param1: 'o'},setFig);
-$("#first").click({param1: 'first'}, preprocess_icon);
-$("#second").click({param1: 'second'}, preprocess_icon);
-$("#third").click({param1: 'third'}, preprocess_icon);
-$("#fourth").click({param1: 'fourth'}, preprocess_icon);
-$("#fifth").click({param1: 'fifth'}, preprocess_icon);
-$("#sixth").click({param1: 'sixth'}, preprocess_icon);
-$("#seventh").click({param1: 'seventh'}, preprocess_icon);
-$("#eighth").click({param1: 'eighth'}, preprocess_icon);
-$("#ninth").click({param1: 'ninth'}, preprocess_icon);
+$("#xbutton").click({param1: 'x'}, initGame);
+$("#obutton").click({param1: 'o'},initGame);
+$("#first").click({param1: 'first'}, preprocess_move);
+$("#second").click({param1: 'second'}, preprocess_move);
+$("#third").click({param1: 'third'}, preprocess_move);
+$("#fourth").click({param1: 'fourth'}, preprocess_move);
+$("#fifth").click({param1: 'fifth'}, preprocess_move);
+$("#sixth").click({param1: 'sixth'}, preprocess_move);
+$("#seventh").click({param1: 'seventh'}, preprocess_move);
+$("#eighth").click({param1: 'eighth'}, preprocess_move);
+$("#ninth").click({param1: 'ninth'}, preprocess_move);
 
 function start() {
     $('#myModal').modal('show');
 }
 
-function firstMove() {
-    $('#fifth').html(game.computer);
-    $('#fifth').removeAttr('onClick');
-}
-
-function setFig(id) {
+function initGame(id) {
   id = id.data.param1;
-  console.log('setFig was called');
-  console.log(id,'is id');
-  if (id == 'x') {
+  console.log('initGame was called');
+  game.moves = 0;
+  if (id === 'x') {
     game.user = '<span>X</span>';
     game.computer = '<span>O</span>';
+    setCurrPl('user')
     console.log('user is x, computer is o');
-  } else if (id == 'o') {
+  } else if (id === 'o') {
     game.user = '<span>O</span>';
     game.computer = '<span>X</span>';
-    console.log('computer is x, user is y');
-    firstMove();
+    console.log('computer is x, user is o');
+    setCurrPl('computer')
+    comp();
     }
-    setCurrPl('user');
 }
 
 function setCurrPl(curr) {
     game.currentPlayer = curr;
 }
 
-function preprocess_icon(id) {
+function preprocess_move(id) {
   id = id.data.param1;
-  icon(id);
+  if ($('#' + id).html() != game.user && $('#' + id).html() != game.computer) {
+    update_board(id);
+  }
 }
 
-function icon(id) {
-    console.log(id, 'is id within icon')
+function update_board(id) {
     if (game.currentPlayer == 'user') {
         $('#' + id).html(game.user);
-        $('#' + id).removeAttr('onClick');
-        gameStatus();
-        setCurrPl('computer');
+        //$('#' + id).removeAttr('onClick');
     }
     else if (game.currentPlayer == 'computer') {
         $('#' + id).html(game.computer);
-        $('#' + id).removeAttr('onClick');
-        gameStatus();
-        setCurrPl('user');
+        //$('#' + id).removeAttr('onClick');
     }
-    game.move++
-    is_it_a_draw();
-    gameStatus();
-
-    if (game.currentPlayer == 'computer') {
-        comp();
+    let status = gameStatus();
+    game.moves++;
+    if (status === false) {
+      console.log(game.moves, ":",status);
+      is_it_a_draw();
+      if (game.currentPlayer == 'user') {
+        setCurrPl('computer');
+        setTimeout(comp, 300);
+      }
+      else {
+        setCurrPl('user');
+      }
+    }
+    else {
+      return;
     }
 }
     
 function comp() {
-    switch (true) {
-        case $('#first').html() != game.user && $('#first').html() != game.computer:
-        icon('first');
+    if (game.moves === 0) {
+      update_board('fifth');
+    }
+    else {
+      switch (true) {
+          case $('#first').html() != game.user && $('#first').html() != game.computer:
+          update_board('first');
+          break;
+          case $('#second').html() !== game.user && $('#second').html() !== game.computer:
+          update_board('second');
+          break;
+          case $('#third').html() !== game.user && $('#third').html() !== game.computer:
+          update_board('third');
+          break;
+          case $('#fourth').html() !== game.user && $('#fourth').html() !== game.computer:
+          update_board('fourth');
+          break;
+          case $('#fifth').html() !== game.user && $('#fifth').html() !== game.computer:
+          update_board('fifth');
+          break;
+          case $('#sixth').html() !== game.user && $('#sixth').html() !== game.computer:
+          update_board('sixth');
+          break;
+          case $('#seventh').html() !== game.user && $('#seventh').html() !== game.computer:
+          update_board('seventh');
+          break;
+          case $('#eighth').html() !== game.user && $('#eighth').html() !== game.computer:
+          update_board('eighth');
+          break;
+          case $('nineth').html() !== game.user && $('#nineth').html() !== game.computer:
+          update_board('nineth');
         break;
-        case $('#second').html() !== game.user && $('#second').html() !== game.computer:
-        icon('second');
-        break;
-        case $('#third').html() !== game.user && $('#third').html() !== game.computer:
-        icon('third');
-        break;
-        case $('#fourth').html() !== game.user && $('#fourth').html() !== game.computer:
-        icon('fourth');
-        break;
-        case $('#fifth').html() !== game.user && $('#fifth').html() !== game.computer:
-        icon('fifth');
-        break;
-        case $('#sixth').html() !== game.user && $('#sixth').html() !== game.computer:
-        icon('sixth');
-        break;
-        case $('#seventh').html() !== game.user && $('#seventh').html() !== game.computer:
-        icon('seventh');
-        break;
-        case $('#eighth').html() !== game.user && $('#eighth').html() !== game.computer:
-        icon('eighth');
-        break;
-        case $('nineth').html() !== game.user && $('#nineth').html() !== game.computer:
-        icon('nineth');
-      break;
-  }
+      }
+    }
 }
+
 
 function gameStatus() {
     var curPlayer;
@@ -123,38 +131,37 @@ function gameStatus() {
       case $('#first').html() === curPlayer && $('#second').html() === curPlayer &&
       $('#third').html() === curPlayer:
         show('#first', '#second', '#third');
-        break;
+        return true;
       case $('#fourth').html() === curPlayer && $('#fifth').html() === curPlayer &&
       $('#sixth').html() === curPlayer:
         show('#fourth', '#fifth', '#sixth');
-        break;
+        return true;
       case $('#seventh').html() === curPlayer && $('#eighth').html() === curPlayer &&
       $('#nineth').html() === curPlayer:
         show('#seventh', '#eighth', '#nineth');
-        break;
+        return true;
       case $('#first').html() === curPlayer && $('#fourth').html() === curPlayer &&
       $('#seventh').html() === curPlayer:
         show('#first', '#fourth', '#seventh');
-        break;
+        return true;
       case $('#second').html() === curPlayer && $('#fifth').html() === curPlayer &&
       $('#eighth').html() === curPlayer:
         show('#second', '#fifth', '#eighth');
-        break;
+        return true;
       case $('#third').html() === curPlayer && $('#sixth').html() === curPlayer &&
       $('#nineth').html() === curPlayer:
         show('#third', '#sixth', '#nineth');
-        break;
+        return true;
       case $('#first').html() === curPlayer && $('#fifth').html() === curPlayer &&
       $('#nineth').html() === curPlayer:
         show('#first', '#fifth', '#nineth');
-        break;
+        return true;
       case $('#third').html() === curPlayer && $('#fifth').html() === curPlayer &&
       $('#seventh').html() === curPlayer:
         show('#third', '#fifth', '#seventh');
-        break;
-      default:
-        is_it_a_draw();
+        return true;
     }
+    return false;
   };
 
   function is_it_a_draw() {
@@ -164,6 +171,7 @@ function gameStatus() {
   }
 
   function lockAll() {
+    //update, does not work
     $('.game-field').removeAttr('Click');
   }
 
@@ -171,16 +179,26 @@ function gameStatus() {
     $(x).addClass('win');
     $(y).addClass('win');
     $(z).addClass('win');
-    lockAll();
+    //lockAll();
     setTimeout(reset, 2000);
   }
 
   function reset() {
     $('.game-field').html('');
-    game.moves = 1;
-    $('.game-field').attr('onClick', 'icon(this.id)');
+    game.moves = 0;
     $('.win').removeClass('win');
-    setTimeout(firstMove, 200);
+    console.log('user ',game.user)
+    if (game.user === '<span>O</span>') {
+      game.user = '<span>X</span>';
+      game.computer = '<span>O</span>';
+      setCurrPl('user')
+    }
+    else {
+      game.user = '<span>O</span>';
+      game.computer = '<span>X</span>';
+      setCurrPl('computer')
+      setTimeout(comp, 200);
+    }
   }
 
 });
